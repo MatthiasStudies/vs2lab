e65e73dd9e346533f2a5164aa3705ead5b1a5a10
Matthias Harzer <matthias.harzer03@gmail.com>
2025-11-06 15:15:37 +0100

    feat (A2): implement async RPC

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 import constRPC
 
 from context import lab_channel
@@ -25,12 +28,20 @@ class Client:
     def stop(self):
         self.chan.leave('client')
 
-    def append(self, data, db_list):
+    def append(self, data, db_list, callback_on_result):
         assert isinstance(db_list, DBList)
         msglst = (constRPC.APPEND, data, db_list)  # message payload
         self.chan.send_to(self.server, msglst)  # send msg to server
         msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+
+        assert msgrcv[1] == constRPC.OK
+
+        def wait():
+            msgrcv = self.chan.receive_from(self.server)  # wait for response
+            callback_on_result(msgrcv[1])  # call the callback with the result
+
+        threading.Thread(target=wait, daemon=True).start()
+        return constRPC.OK
 
 
 class Server:
@@ -39,10 +50,22 @@ class Server:
         self.server = self.chan.join('server')
         self.timeout = 3
 
+
+
     @staticmethod
     def append(data, db_list):
         assert isinstance(db_list, DBList)  # - Make sure we have a list
-        return db_list.append(data)
+        time.sleep(5)
+        return db_list.append(data)  # - Append data to list and return new list
+
+    def wrap(self, fn, client):
+        def run():
+            result = fn()
+            self.chan.send_to({client}, result)
+
+        threading.Thread(target=run, daemon=True).start()
+        return constRPC.OK
+
 
     def run(self):
         self.chan.bind(self.server)
@@ -52,7 +75,7 @@ class Server:
                 client = msgreq[0]  # see who is the caller
                 msgrpc = msgreq[1]  # fetch call & parameters
                 if constRPC.APPEND == msgrpc[0]:  # check what is being requested
-                    result = self.append(msgrpc[1], msgrpc[2])  # do local call
+                    result = self.wrap(lambda: self.append(msgrpc[1], msgrpc[2]), client)
                     self.chan.send_to({client}, result)  # return response
                 else:
-                    pass  # unsupported request, simply ignore
+                    pass
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,3 +1,8 @@
+import threading
+import time
+from pprint import pprint
+
+import constRPC
 import rpc
 import logging
 
@@ -8,9 +13,30 @@ lab_logging.setup(stream_level=logging.INFO)
 cl = rpc.Client()
 cl.run()
 
-base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+result: rpc.DBList | None = None
 
-print("Result: {}".format(result_list.value))
+def on_response(_result):
+    global result
+    result = _result
+    print("Callback Response:")
+    pprint(result)
+
+base_list = rpc.DBList({'foo'})
+
+start = time.time()
+response = cl.append('bar', base_list, on_response)
+print("Response:", response)
+
+print("Waiting for result", end='', flush=True)
+while result is None:
+    print('.', end='', flush=True)
+    time.sleep(0.5)
+
+stop = time.time()
+
+print()
+
+print("Completed in {:.2f} seconds".format(stop - start))
+print("Result: {}".format(result.value))
 
 cl.stop()
