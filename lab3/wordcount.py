abd3bef47ba5ed73aae9903d716906e54b837c1c
Matthias Harzer <matthias.harzer03@gmail.com>
2025-12-19 10:46:30 +0100

    feat(A3): implement zmq based

--- lab3/wordcount.py
+++ lab3/wordcount.py
@@ -1,136 +1,104 @@
-import time
-from dataclasses import dataclass
-import threading
 import zmq
+import multiprocessing
+import time
 
-print_q = []
-def thread_safe_print(*args, **kwargs):
-    global print_q
-    print_q += [(args, kwargs)]
+NUM_MAPPERS = 3
+NUM_REDUCERS = 2
+SPLITTER_PORT = 5555
+REDUCER_PORTS = [5556, 5557]
 
-def process_print_queue():
-    global print_q
+def splitter(filename):
+    context = zmq.Context()
+    sender = context.socket(zmq.PUSH)
+    sender.bind(f"tcp://*:{SPLITTER_PORT}")
+
+    time.sleep(1)
+
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            sender.send_string(line.strip())
+
+    for _ in range(NUM_MAPPERS):
+        sender.send_string("__EOF__")
+
+    sender.close()
+    context.term()
+
+def mapper():
+    context = zmq.Context()
+
+    receiver = context.socket(zmq.PULL)
+    receiver.connect(f"tcp://localhost:{SPLITTER_PORT}")
+
+    reducer_sockets = []
+    for port in REDUCER_PORTS:
+        sender = context.socket(zmq.PUSH)
+        sender.connect(f"tcp://localhost:{port}")
+        reducer_sockets.append(sender)
+
     while True:
-        if print_q:
-            args, kwargs = print_q.pop(0)
-            print(*args, **kwargs)
+        message = receiver.recv_string()
 
+        if message == "__EOF__":
+            for sock in reducer_sockets:
+                sock.send_string("__EOF__")
+            break
 
-class Splitter:
-    def __init__(self, file: str, mapper_addresses: list[str]):
-        self.file = file
-        context = zmq.Context()
-        self.mapper_sockets: list[zmq.Socket] = []
+        words = message.split()
+        for word in words:
+            reducer_idx = hash(word) % NUM_REDUCERS
+            target_socket = reducer_sockets[reducer_idx]
+            target_socket.send_string(word)
 
-        for address in mapper_addresses:
-            socket = context.socket(zmq.PUSH)
-            socket.connect(address)
-            self.mapper_sockets.append(socket)
+    receiver.close()
+    for sock in reducer_sockets:
+        sock.close()
+    context.term()
 
-    def split_and_send(self):
-        current_socket_index = 0
-        with open(self.file, 'r', encoding='utf-8') as f:
-            for line in f:
-                socket_info = self.mapper_sockets[current_socket_index]
-                socket_info.send_string(line.strip())
-                current_socket_index = (current_socket_index + 1) % len(self.mapper_sockets)
+def reducer(reducer_id, port):
+    context = zmq.Context()
+    receiver = context.socket(zmq.PULL)
+    receiver.bind(f"tcp://*:{port}")
 
-        for socket in self.mapper_sockets:
-            socket.send_string("__EOF__")  # Send termination signal
+    counts = {}
+    mappers_finished_count = 0
 
-class Reducer:
-    def __init__(self, address: str, number: int):
-        context = zmq.Context()
-        self.socket = context.socket(zmq.PULL)
-        self.socket.bind(address)
-        self.word_count = {}
-        self.number = number
+    while True:
+        message = receiver.recv_string()
 
-    def reduce(self):
-        while True:
-            word = self.socket.recv_string()
-            if word == "__EOF__":
+        if message == "__EOF__":
+            mappers_finished_count += 1
+            if mappers_finished_count == NUM_MAPPERS:
                 break
-            if word in self.word_count:
-                self.word_count[word] += 1
-            else:
-                self.word_count[word] = 1
+        else:
+            word = message
+            counts[word] = counts.get(word, 0) + 1
+            print(f" -> [{reducer_id}] '{word}': {counts[word]}")
 
-            thread_safe_print(f"[{self.number}] Word '{word}' count: {self.word_count[word]}")
+    print(f"\nReducer {reducer_id} final wordcount:")
+    for word, count in counts.items():
+        print(f"{word}: {count}")
+    print("----------------------------------\n")
 
-
-
-class Mapper:
-    def __init__(self, address: str, reducer_addresses: list[str]):
-        context = zmq.Context()
-        self.socket = context.socket(zmq.PULL)
-        self.socket.bind(address)
-        self.reducer_sockets: list[zmq.Socket] = []
-        for address in reducer_addresses:
-            sock = context.socket(zmq.PUSH)
-            sock.connect(address)
-            self.reducer_sockets.append(sock)
-
-    def word_to_reducer(self, word: str) -> zmq.Socket:
-        index = hash(word) % len(self.reducer_sockets)
-        return self.reducer_sockets[index]
-
-    def map(self):
-        while True:
-            line = self.socket.recv_string()
-            if line == "__EOF__":
-                for socket in self.reducer_sockets:
-                    socket.send_string("__EOF__")  # Send termination signal to reducers
-                break
-
-            words = line.split()
-            for word in words:
-                socket = self.word_to_reducer(word)
-                socket.send_string(word)
+    receiver.close()
+    context.term()
 
 if __name__ == "__main__":
-    reducer_addresses = [
-        f"tcp://127.0.0.1:5001",
-        f"tcp://127.0.0.1:5002",
-    ]
-    mapper_addresses = [
-        f"tcp://127.0.0.1:6001",
-        f"tcp://127.0.0.1:6002",
-        f"tcp://127.0.0.1:6003",
-    ]
+    processes = []
 
-    reducers = []
-    for i, address in enumerate(reducer_addresses):
-        reducers.append(Reducer(address, i))
+    for i in range(NUM_REDUCERS):
+        p = multiprocessing.Process(target=reducer, args=(i, REDUCER_PORTS[i]))
+        p.start()
+        processes.append(p)
 
-    mappers= []
-    for address in mapper_addresses:
-        mappers.append(Mapper(address, reducer_addresses))
+    for i in range(NUM_MAPPERS):
+        p = multiprocessing.Process(target=mapper)
+        p.start()
+        processes.append(p)
 
-    splitter = Splitter("input.txt", mapper_addresses)
+    p_splitter = multiprocessing.Process(target=splitter, args=("input.txt",))
+    p_splitter.start()
+    processes.append(p_splitter)
 
-    t_split = threading.Thread(target=splitter.split_and_send, daemon=True)
-    t_mappers = []
-    t_reducers = []
-    for mapper in mappers:
-        t_mappers.append(threading.Thread(target=mapper.map, daemon=True))
-    for reducer in reducers:
-        t_reducers.append(threading.Thread(target=reducer.reduce, daemon=True))
-
-    t_print = threading.Thread(target=process_print_queue, daemon=True)
-    t_print.start()
-
-    threads = [t_split] + t_mappers + t_reducers
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    time.sleep(1)  # Allow time for all prints to be processed
-    print("Final word counts:")
-    for i, reducer in enumerate(reducers):
-        print(f"Reducer {i} counts:")
-        for word, count in reducer.word_count.items():
-            print(f"  {word}: {count}")
-
+    for p in processes:
+        p.join()
